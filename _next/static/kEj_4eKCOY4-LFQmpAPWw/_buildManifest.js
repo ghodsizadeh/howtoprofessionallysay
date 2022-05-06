4ab57a9e735115c21663b9cadd721ea4a5c22985
Mehdi Ghodsizadeh <mehdi.ghodsizadeh@gmail.com>
2022-05-06 19:24:05 +0430

    Deploy gh-pages

---
+++ _next/static/kEj_4eKCOY4-LFQmpAPWw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-665d33ba26095656.js"],"/_error":["static/chunks/pages/_error-a9f36ce656a10e9d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
